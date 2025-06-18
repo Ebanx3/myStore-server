8313a19f1e313f6e418f6f7e1282a8fe3555ef18
Ebanx3 <esteban.dos.santos@gmail.com>
2025-06-17 22:13:01 -0300

    corrections to schemas

--- src/services/zod/schemas.ts
+++ src/services/zod/schemas.ts
@@ -1,28 +1,28 @@
 import { z } from "zod/v4";
 
 const StoreSchema = z.object({
-  name: z.string().min(1, "Name is required"),
+  name: z.string().min(1, "El nombre es requerido"),
   maxProducts: z.number().min(50).optional().default(50),
 });
 
 const ProductSchema = z.object({
-  name: z.string().min(1, "Name is required"),
-  details: z.string().min(5, "Details must have at least 5 characters"),
-  price: z.number().positive("Price must be greater than 0"),
-  stock: z.number().int().nonnegative("Stock can not be negative"),
+  name: z.string().min(1, "El nombre es requerido"),
+  details: z.string().min(5, "Detalles debe contener al menos 5 caracteres"),
+  price: z.number().positive("El precio debe ser mayor a 0"),
+  stock: z.number().int().nonnegative("El stock no puede ser negativo"),
   statusVisible: z.boolean().optional().default(true)
 });
 
 const SignupSchema = z.object({
-  name: z.string().min(3, 'Name must have at least 3 characters').max(40, 'Name can not have more than 40 characters'),
-  lastname : z.string().min(3, 'Lastname must have at least 3 characters').max(40, 'Name can not have more than 40 characters'),
+  name: z.string().regex(/^[\p{L}]{3,30}$/u, 'El nombre debe contener entre 3 y 30 letras'),
+  lastname : z.string().regex(/^[\p{L}]{3,30}$/u, 'El apellido debe contener entre 3 y 30 letras'),
   email: z.string().regex(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, 'Email format invalid'),
-  password: z.string().min(8, 'Password must have at least 8 characters').max(50, 'Password can not have more than 50 characters')
+  password: z.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{12,}$/, 'La contraseña debe contener al menos 12 caracteres, incluyendo mayúscula, minúscula y al menos un número')
 });
 
 const LoginSchema = z.object({
   email: z.string().regex(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, 'Email format invalid'),
-  password: z.string().min(8, 'Password must have at least 8 characters').max(50, 'Password can not have more than 50 characters')
+  password: z.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{12,}$/, 'La contraseña debe contener al menos 12 caracteres, incluyendo mayúscula, minúscula y al menos un número')
 })
 
 export const Schemas = {
